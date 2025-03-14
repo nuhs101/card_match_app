d4a893ef93abc590031bd6b78ee84ea1f6d39b0d
“Staxupgit config --global user.email “nuhshekhey@gmail.com <nuhshekhey@gmail.com>
2025-03-14 14:37:50 -0400

    basic structure for Card Matching Game with grid layout and restart button functionality

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:card_match_game/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(CardMatchingGame());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
